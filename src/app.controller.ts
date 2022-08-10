119d0510ddda6d177362a6e581b3b319c49cf41f
smboy86 <smboy86@naver.com>
2022-08-10 10:39:28 +0900

    dev - 프리즈마 조회 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/appinfo')
+  getAppInfo() {
+    return this.appService.getAppInfo();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getAppInfo() {
+    return this.prisma.appInfo.findMany();
   }
 }
