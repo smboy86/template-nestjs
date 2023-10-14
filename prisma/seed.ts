3b70a359d4c66b8563cbadf9bf73fa9e51713c1f
devpoi <smboy86@naver.com>
2023-10-14 13:20:02 +0900

    update - prisma 코드 클린, 버전 업데이트

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,26 +1,43 @@
 import { PrismaClient } from '@prisma/client';
 
-// initialize the Prisma Client
 const prisma = new PrismaClient();
 
 async function main() {
-  // create two dummy articles
-  const testInsert = await prisma.appInfo.upsert({
+  // #) 시드 생성시 중복 에러를 피하기 위해 upsert 커맨드 이용
+  // 1) create appInfo data
+  const insertAppInfo = await prisma.appInfo.upsert({
     where: { appName: 'template-nestjs app' },
-    update: {},
+    update: {
+      regDt: new Date(),
+      modDt: new Date(),
+    },
     create: {
       appName: 'template-nestjs app',
       text: '재미난 앱을 만들어 보입시다 ~',
     },
   });
+  console.log('insert AppInfo', insertAppInfo);
 
-  console.log({ testInsert });
+  // 2) create temp user
+  const insertUser = await prisma.user.upsert({
+    where: { email: 'test-a@email.com' },
+    update: {
+      regDt: new Date(),
+      modDt: new Date(),
+    },
+    create: {
+      email: 'test-a@email.com',
+      password: 'qwe123',
+      name: 'seed-user',
+    },
+  });
+  console.log('insert User', insertUser);
 }
 
 // execute the main function
 main()
   .catch((e) => {
-    console.error(e);
+    console.error('seed errrr  ::: ', e);
     process.exit(1);
   })
   .finally(async () => {
